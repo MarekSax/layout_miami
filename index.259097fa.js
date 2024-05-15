c57fd916606b42dc2f7438a9bfbe5dd4b563f449
MarekSax <mareksax@wp.pl>
2024-05-15 11:40:04 +0200

    make build

---
+++ index.259097fa.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){console.log("test"),"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault()});
+//# sourceMappingURL=index.259097fa.js.map
